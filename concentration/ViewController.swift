a171597f4b530381b165348ba1527ad1ee1c2af9
Grace Lu Li <lu.li.azure@gmail.com>
2018-11-17 11:44:56 -0800

    Lecture1 - project concentrationl.

---
+++ concentration/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  concentration
+//
+//  Created by Lu Li on 8/20/18.
+//  Copyright © 2018 Grace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃", "👻", "🎃","👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("choosed card is not in cardButton");
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal);
+            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal);
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
+
+}
+
